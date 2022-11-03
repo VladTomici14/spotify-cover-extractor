4ed1c07cc852a1fc4dabf388fa96cacddfe3b734
VladTomici14 <vladtomici@gmail.com>
2022-11-03 12:48:41 +0200

    Deleted the file.

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-class Utils:
-    def formatString(self, string):
-        new = string.replace('!', '')
-        print(new)
-
-if __name__ == "__main__":
-    Utils.formatString('Wow! Cool Album!')
